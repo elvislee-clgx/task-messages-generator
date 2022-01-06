52434d5e33061d4c137078730eda1a220db2d271
Elvis Lee <polee@corelogic.com>
2022-01-05 17:32:06 -0800

    Add cf command to login and restart task launcher

---
+++ gitpython_push.py
@@ -0,0 +1,15 @@
+from git import Repo
+import sys
+
+def git_push(git_repo_path, env, pipeline_title):
+    try:
+        repo = Repo(git_repo_path)
+        repo.git.add(update=True)
+        repo.index.commit("Add task message \"{pipeline_title}\" in {env} ".format(env=env.upper(), pipeline_title=pipeline_title))
+
+        origin = repo.remote(name='origin')
+        origin.push()
+        print('A new task message [{pipeline_title}] in {env} is created successfully!'.format(env=env.upper(),pipeline_title=pipeline_title))
+
+    except:
+        print('Some error occurred while pushing the code')
--- task_messages_generator.py
+++ task_messages_generator.py
@@ -1,7 +1,8 @@
 import os
 import sys
+from gitpython_push import git_push
 
-def generate_task_message(env, pipeline_name, repo_path):
+def generate_task_message(env, pipeline_name, repo_path, username, password):
     env = env.lower()
     pipeline_folder_name = pipeline_name.strip().lower().replace(' ', '-')
     pipeline_title = pipeline_name.replace('-', ' ').title()
@@ -11,9 +12,13 @@ def generate_task_message(env, pipeline_name, repo_path):
     if env == 'dev':
         gcs_config_file_path = "gs://idap-dev-aux-scripts-infrastructure-data/test/enriched/dataflow-bigquery-elastic/config/{pipeline_folder_name}/{env}".format(env=env, pipeline_folder_name=pipeline_folder_name)
         file_object = open(local_repo_folder_path + '/idap-kafka-to-scdf-task-launcher-dev-west.yml', 'r+')
+        pcf_url = 'api.sys.pcfusw1dev.solutions.corelogic.com'
+        pcf_org = 'Idap_data_api_us-dev'
     elif env == 'int':
         gcs_config_file_path = "gs://idap-preprod-aux-scripts-infrastructure-data/int/enriched/dataflow-bigquery-elastic/config/{pipeline_folder_name}/{env}".format(env=env, pipeline_folder_name=pipeline_folder_name)
         file_object = open(local_repo_folder_path + '/idap-kafka-to-scdf-task-launcher-int-west.yml', 'r+')
+        pcf_url = 'api.sys.pcfusw1stg.solutions.corelogic.com'
+        pcf_org = 'Idap_data_api_us-int'
 
     doc_str = file_object.read()
 
@@ -26,8 +31,11 @@ def generate_task_message(env, pipeline_name, repo_path):
                         '        command: .java-buildpack/open_jdk_jre/bin/java -Dspring.profiles.active=dataapi-{}-west org.springframework.boot.loader.JarLauncher --gcs-config-file="'.format(env) + gcs_config_file_path + '/incremental-job-parameters.yml"\n' +
                         '        memory: 4096\n    ')
 
-    insert_position = doc_str.find("\"[AVM snapshot completed FULL]\":")
+    if doc_str.find(pipeline_title) != -1:
+        file_object.close()
+        raise ValueError("This pipeline task message is already in the .yml file")
 
+    insert_position = doc_str.find("\"[AVM snapshot completed FULL]\":")
     if insert_position == -1:
         file_object.close()
         raise ValueError("The insert position cannot be found")
@@ -36,7 +44,10 @@ def generate_task_message(env, pipeline_name, repo_path):
     file_object.seek(0)
     file_object.write(doc_add_task_message)
     file_object.close()
-    os.system('python3 gitpython-push.py {repo_git_folder} {pipeline_name}'.format(repo_git_folder=repo_git_folder, pipeline_name=pipeline_name))
+    git_push(repo_git_folder, env, pipeline_title)
+
+    os.system('cf login -a {pcf_url} -u {username} -p {password} -o {pcf_org} -s Dataloading'.format(pcf_url=pcf_url, username=username, password=password, pcf_org=pcf_org))
+    os.system('cf restart idap-kafka-to-scdf-task-launcher')
 
 # Parameters: 1.environment 2. pipeline name 3. the absolute path to local repo
-generate_task_message('dev', 'test-dummy-job3', '/Users/polee/IdeaProjects/dummy-idap-dataapi-cloud-config')
+generate_task_message('int', 'test-dummy-job4', '/Users/polee/IdeaProjects/dummy-idap-dataapi-cloud-config', 'polee@corelogic.com', 'HopeFuture#2911')
